77a780d3d08157094a10a97d3c56ac62e34683d9
Rishi Goswamy <rishigoswamy@Rishis-MacBook-Pro.local>
2022-12-10 21:55:59 -0500

    Node changes

--- Controllers/Test/test-controller.js
+++ Controllers/Test/test-controller.js
@@ -7,5 +7,9 @@ const TestController = (app) => {
         const port = req.headers.host.split(':')[1];
         res.json({"status": 'Base Level Controller API is up and running!', port})
     });
+    app.get('/', (req, res) => {
+        const port = req.headers.host.split(':')[1];
+        res.json({"status": 'Node application is up and running!', port})
+    })
 }
 export default TestController;
